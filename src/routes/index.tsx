022bcdf10eab75d3a845a5330f08ec90e4151dd6
mainpro <mainpro@126.com>
2025-11-13 16:56:55 +0800

    fix 修改为HashRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import App from '../App';
 import RentalOrderDetail from '../pages/RentalOrderDetail';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
